f1a6b42cd1475a938459a5d82a8dfd0d5cbb9665
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-10 14:23:12 +0300

    add new app ui

--- src/main/java/ui/MainMenu.java
+++ src/main/java/ui/MainMenu.java
@@ -1,34 +1,39 @@
 package ui;
 
 import dao.daoimpl.ExamImpl;
-
-import java.sql.SQLException;
 import java.util.Scanner;
 
 public class MainMenu {
-    public static void start() throws SQLException {
+    public static void start() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("1. Sign up as new student\n2. Log in student\n3. System menu");
+            System.out.println("" +
+                    "1. Sign up as new Applicant\n" +
+                    "2. Applicant Sign in\n" +
+                    "3. Test Area\n" +
+                    "4. Admin login\n" +
+                    "0. Exit");
             switch (scanner.nextInt()) {
                 case 1:
-                    System.out.println("create new stud");
+                    System.out.println("Sign up as new Applicant");
                     break;
                 case 2:
-                    System.out.println("login stud");
+                    System.out.println("Applicant sign in window");
                     break;
                 case 3:
                     System.out.println("test-area");
                     ExamImpl ex = new ExamImpl();
                     System.out.println(ex.getAll());
                     System.out.println(ex.getById(10));
-
                     break;
+                case 4:
+                    break;
+                case 0:
+                    System.out.println("Good bye! See you later!");
+                    System.exit(0);
                 default:
                     System.out.println("Wrong choice");
             }
         }
-
-
     }
 }
