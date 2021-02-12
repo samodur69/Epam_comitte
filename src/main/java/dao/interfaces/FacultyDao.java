4f253545bcdad27ed6da9aaa08377a9885bdc32e
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-12 17:07:25 +0300

    add getAll methods

---
+++ src/main/java/dao/interfaces/FacultyDao.java
@@ -0,0 +1,9 @@
+package dao.interfaces;
+
+import dao.model.Faculty;
+
+public interface FacultyDao extends BaseDao<Faculty> {
+    void setCapacity(String facultyName);
+    void setMinGrade(String facultyName);
+    void getNameById(int id);
+}
