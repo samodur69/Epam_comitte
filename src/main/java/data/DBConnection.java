cc99e31c5d50dc8be032569d2131a8241d511cfb
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-12 17:05:09 +0300

    add Logger for DB events

--- src/main/java/data/DBConnection.java
+++ src/main/java/data/DBConnection.java
@@ -3,16 +3,16 @@ package data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
+import java.sql.*;
 
 public class DBConnection {
 
     private final String DB_URL="jdbc:oracle:thin:@firsttestbase_medium?TNS_ADMIN=/Users/apple/_Projects/Epam_comitte/src/main/resources/Wallet_FirstTestBase";
     private final String DB_USER = "ADMIN";
-    private final String DB_PASSWORD = "Loveloveyou87";
+    private final String DB_PASSWORD = "Kethum,Bashum21";
+//    private final String DB_USER = "EPAM1";
+//    private final String DB_PASSWORD = "AdmissionComitte1";
     private static final Logger logger = LoggerFactory.getLogger(DBConnection.class);
     private static DBConnection instance;
 
@@ -28,7 +28,7 @@ public class DBConnection {
     public static Connection getConnection() {
         if (instance == null) {
             instance = new DBConnection();
-            System.out.println("New DB connection created");
+            logger.info("New DB connection created");
         }
         try {
             return DriverManager.getConnection(instance.DB_URL, instance.DB_USER, instance.DB_PASSWORD);
@@ -43,7 +43,7 @@ public class DBConnection {
         try {
             if (connection != null) {
                 connection.close();
-                connection = null;
+//                connection = null;
             }
         } catch (SQLException e) {
             System.out.println("Troubles with closing connection");
@@ -51,10 +51,44 @@ public class DBConnection {
         }
     }
 
-    public static void close(PreparedStatement ps, Connection conn) {
+    public static void close(ResultSet resultSet, PreparedStatement ps, Connection conn) {
         try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
             if (ps != null) {
                 ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Troubles with closing connection");
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet resultSet, Statement statement, Connection conn) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Troubles with closing connection");
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(PreparedStatement preparedStatement, Connection conn) {
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
             }
             if (conn != null) {
                 conn.close();
