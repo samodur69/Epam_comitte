1e5d364f0af20e000922558e7dbde311ae99c87b
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-12 17:06:34 +0300

    add some new methods

--- src/main/java/dao/implementations/ApplicantImpl.java
+++ src/main/java/dao/implementations/ApplicantImpl.java
@@ -3,22 +3,24 @@ package dao.implementations;
 import dao.interfaces.ApplicantDao;
 import dao.model.Applicant;
 import data.DBConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicantImpl implements ApplicantDao {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApplicantImpl.class);
+
     @Override
     public void create(Applicant applicant) {
         String sqlCreate = "INSERT INTO STUDENTS " +
-                "(FIRST_NAME, LAST_NAME, SCHOOL_AVERAGE, FACULTY_ID, ST_PASSWORD) VALUES (?, ?, ?, " +
-                "SELECT FACULTY_ID FROM FACULTY_LIST WHERE FACULTY_NAME = ?), ?)";
+                "(FIRST_NAME, LAST_NAME, SCHOOL_AVG_SCORES, FACULTY_ID, ST_PASSWORD, ST_EMAIL) VALUES (?, ?, ?, (" +
+                "SELECT FACULTY_ID FROM FACULTY_LIST WHERE FACULTY_NAME = ?), ?, ?)";
         Connection conn = null;
         PreparedStatement ps = null;
-        applicant.setPassword(Applicant.generatePin());
         System.out.println(applicant.getPassword());
         try {
             conn = DBConnection.getConnection();
@@ -28,10 +30,49 @@ public class ApplicantImpl implements ApplicantDao {
             ps.setInt(3, applicant.getSchoolAverage());
             ps.setString(4, "Mathematics"); // TODO insert id. set faculty id in model
             ps.setString(5, applicant.getPassword());
+            ps.setString(6, applicant.getEmail());
+            ps.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Can`t add new applicant to DB");
+            logger.info("Can`t add new applicant to DB");
             e.printStackTrace();
+        } finally {
+            DBConnection.close(ps, conn);
         }
+    }
+
+    @Override
+    public List<Applicant> getAll() {
+        List<Applicant> applicants = new ArrayList<>();
+        Applicant applicant;
+        String sqlGetAll = "SELECT * FROM STUDENTS";
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DBConnection.getConnection();
+            st = conn.createStatement();
+            st.execute(sqlGetAll);
+            rs = st.getResultSet();
+            while (rs.next()) {
+                applicant = new Applicant();
+                applicant.setId(rs.getInt("ID"));
+                applicant.setFirstName(rs.getString("FIRST_NAME"));
+                applicant.setLastName(rs.getString("LAST_NAME"));
+                applicant.setSchoolAverage(rs.getInt("SCHOOL_AVG_SCORES"));
+                applicant.setFacultyId(rs.getInt("FACULTY_ID"));
+                applicant.setEnrolled(rs.getString("ENROLLED"));
+                applicant.setPassword(rs.getString("ST_PASSWORD"));
+                applicant.setEmail(rs.getString("ST_EMAIL"));
+                applicants.add(applicant);
+//                System.out.println(applicant.toString());
+            }
+        } catch (SQLException e) {
+            logger.info("Troubles with getting list of all Applicants");
+            e.printStackTrace();
+        } finally {
+            DBConnection.close(rs, st, conn);
+        }
+        return applicants;
     }
 
     @Override
@@ -44,10 +85,7 @@ public class ApplicantImpl implements ApplicantDao {
         return null;
     }
 
-    @Override
-    public List<Applicant> getAll() {
-        return null;
-    }
+
 
     @Override
     public Applicant getById(int id) {
---
+++ src/main/java/dao/implementations/FacultyImpl.java
@@ -0,0 +1,78 @@
+package dao.implementations;
+
+import dao.interfaces.FacultyDao;
+import dao.model.Faculty;
+import data.DBConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FacultyImpl implements FacultyDao {
+
+    @Override
+    public List getAll() {
+        List<Faculty> faults = new ArrayList<>();
+        Faculty faculty;
+        String sqlGetAll = "SELECT * FROM FACULTY_LIST";
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DBConnection.getConnection();
+            st = conn.createStatement();
+            st.execute(sqlGetAll);
+            rs = st.getResultSet();
+            while (rs.next()) {
+                faculty = new Faculty();
+                faculty.setFacultyId(rs.getInt("FACULTY_ID"));
+                faculty.setFacultyName(rs.getString("FACULTY_NAME"));
+                faculty.setFacultyCapacity(rs.getInt("FACULTY_CAPACITY"));
+                faculty.setMinGrade(rs.getInt("FACULTY_MIN_GRADE"));
+                faults.add(faculty);
+//                System.out.println(faculty.toString());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return faults;
+    }
+
+    @Override
+    public Faculty getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(Faculty faculty) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void create(Faculty faculty) {
+
+    }
+
+    @Override
+    public void setCapacity(String facultyName) {
+
+    }
+
+    @Override
+    public void setMinGrade(String facultyName) {
+
+    }
+
+    @Override
+    public void getNameById(int id) {
+
+    }
+}
