2a72df3ac7db559aa07de49ef3f8f53ad8b94e25
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-15 17:34:08 +0300

    add getIdByNAme method. Testing ok

--- src/main/java/dao/implementations/ExamImpl.java
+++ src/main/java/dao/implementations/ExamImpl.java
@@ -3,24 +3,27 @@ package dao.implementations;
 import dao.interfaces.ExamDao;
 import data.DBConnection;
 import dao.model.Exam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 import java.util.List;
 
 public class ExamImpl implements ExamDao {
+    private static final Logger logger = LoggerFactory.getLogger(ExamImpl.class);
 
     @Override
-    public List getAll() {
+    public List<Exam> getAll() {
         return null;
     }
 
     @Override
-    public Object getById(int id) {
+    public Exam getById(int id) {
         return null;
     }
 
     @Override
-    public void update(Object obj) {
+    public void update(Exam obj) {
 
     }
 
@@ -30,6 +33,31 @@ public class ExamImpl implements ExamDao {
     }
 
     @Override
-    public void create(Object obj) {
+    public void create(Exam obj) {
+
+    }
+
+    @Override
+    public int getIdByName(String examName) {
+        String sqlGetId = "SELECT EXAM_ID FROM EXAMS_LIST WHERE EXAM_NAME = ?";
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int examId = 0;
+        try {
+            conn = DBConnection.getConnection();
+            ps = conn.prepareStatement(sqlGetId);
+            ps.setString(1, examName);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                examId = rs.getInt("EXAM_ID");
+            }
+        } catch (SQLException | NullPointerException e) {
+            logger.info("Bug when get Exam id by it name");
+            e.printStackTrace();
+        } finally {
+            DBConnection.close(rs, ps, conn);
+        }
+        return examId;
     }
 }
