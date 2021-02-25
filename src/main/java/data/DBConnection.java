4f7a824d66d74035985b3aece1f77d38e931dec0
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-25 17:18:45 +0300

    delete garbage

--- src/main/java/data/DBConnection.java
+++ src/main/java/data/DBConnection.java
@@ -2,24 +2,16 @@ package data;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.GetProperties;
-
-
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.*;
-import java.util.Properties;
 
 public class DBConnection {
 
     private final String DB_URL="jdbc:oracle:thin:@firsttestbase_medium?TNS_ADMIN=/Users/apple/_Projects/Epam_comitte/src/main/resources/Wallet_FirstTestBase";
     private final String DB_USER = "ADMIN";
     private final String DB_PASSWORD = "AdmissionCommitte1";
-//    private String DB_URL;
-//    private String DB_USER;
-//    private String DB_PASSWORD;
     private static final Logger logger = LoggerFactory.getLogger(DBConnection.class);
     private static DBConnection instance;
+
 
     private DBConnection() {
         try {
