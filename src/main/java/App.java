322c1e18a434205d35a153c490293f458806337f
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-26 13:59:11 +0300

    add sout to detect DB connection.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         Connection connection = DBConnection.getConnection();
+        System.out.println("TTTTTTTTEEEEESSSSSTTTT");
         try {
             MainMenu.start();
         } catch (Exception e) {
