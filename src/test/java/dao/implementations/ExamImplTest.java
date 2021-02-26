aaeebae27455415cf2ce9f45f01b4b3ac64b2cf5
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-02-26 13:52:28 +0300

    add test methods

--- src/test/java/dao/implementations/ExamImplTest.java
+++ src/test/java/dao/implementations/ExamImplTest.java
@@ -1,109 +1,119 @@
 package dao.implementations;
 
 import dao.model.Exam;
-import org.testng.annotations.BeforeMethod;
+import data.DBConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
-public class ExamImplTest {
+public class ExamImplTest extends BaseTest{
 
     private ExamImpl examImplUnderTest;
+    private static final Logger logger = LoggerFactory.getLogger(ExamImplTest.class);
+    private Statement st;
+    private Connection conn;
+    private List<Exam> examsList;
 
-    @BeforeMethod
-    public void setUp() {
+
+
+    @BeforeClass
+    public void startSetUp() throws SQLException {
         examImplUnderTest = new ExamImpl();
-    }
-
-    @Test
-    public void testGetAll() {
-        // Setup
-        final Exam exam = new Exam();
-        exam.setExamId(0);
-        exam.setExamName("examName");
-        final List<Exam> expectedResult = List.of(exam);
-
-        // Run the test
-        final List<Exam> result = examImplUnderTest.getAll();
-
-        // Verify the results
-        assertEquals(expectedResult , result);
-    }
-
-    @Test
-    public void testGetById() {
-        // Setup
-        final Exam expectedResult = new Exam();
-        expectedResult.setExamId(0);
-        expectedResult.setExamName("examName");
-
-        // Run the test
-        final Exam result = examImplUnderTest.getById(0);
-
-        // Verify the results
-        assertEquals(expectedResult , result);
-    }
-
-    @Test
-    public void testUpdate() {
-        // Setup
-        final Exam exam = new Exam();
-        exam.setExamId(0);
-        exam.setExamName("examName");
-
-        // Run the test
-        final int result = examImplUnderTest.update(exam);
-
-        // Verify the results
-        assertEquals(0 , result);
-    }
-
-    @Test
-    public void testDelete() {
-        // Setup
-
-        // Run the test
-        final int result = examImplUnderTest.delete(0);
-
-        // Verify the results
-        assertEquals(0 , result);
+        conn = DBConnection.getConnection();
+        st = conn.createStatement();
     }
 
     @Test
     public void testCreate() {
-        // Setup
-        final Exam exam = new Exam();
-        exam.setExamId(0);
-        exam.setExamName("examName");
-
-        // Run the test
+        final Exam exam = new Exam("TestExam");
         final int result = examImplUnderTest.create(exam);
-
-        // Verify the results
-        assertEquals(0 , result);
+        assertTrue(result > 0);
+        // TODO check created exam
     }
 
     @Test
-    public void testGetIdListByFacultyId() {
-        // Setup
+    public void testGetAll() throws SQLException {
+        int expectedResult = 0;
+        ResultSet rs = st.executeQuery("SELECT COUNT(exam_id) FROM EXAMS_LIST");
+        if (rs.next()) {
+            expectedResult = rs.getInt(1);
+        }
+        final List<Exam> examsList = examImplUnderTest.getAll();
+        assertEquals(expectedResult , examsList.size());
+    }
 
-        // Run the test
-        final List<Integer> result = examImplUnderTest.getIdListByFacultyId(0);
+    @Test(dependsOnMethods = {"testGetAll"})
+    public void testGetById() {
+        SoftAssert softAssert = new SoftAssert();
+        List<Exam> examList = examImplUnderTest.getAll();
+        for (Exam el: examList) {
+            int id = el.getExamId();
+            Exam result = examImplUnderTest.getById(id);
+            softAssert.assertEquals(el, result);
+        }
+        softAssert.assertAll();
+    }
 
-        // Verify the results
-        assertEquals(List.of(0) , result);
+    @Test (dependsOnMethods = {"testCreate"})
+    public void testUpdate() {
+        List<Exam> examList = examImplUnderTest.getAll();
+        Exam examUpdate = new Exam();
+        if (examList.size() > 1) {
+            examUpdate = examList.get(0);
+        }
+        examUpdate.setExamName("UpdateTest");
+        final int result = examImplUnderTest.update(examUpdate);
+        examList = examImplUnderTest.getAll();
+        boolean updated = false;
+        for (Exam el: examList) {
+            if (el.getExamName().equals(examUpdate.getExamName())) {
+                updated = true;
+            }
+        }
+        assertTrue(updated);
+    }
+
+    @Test
+    public void testDelete() throws SQLException {
+        ResultSet rs = st.executeQuery("SELECT COUNT(EXAM_ID), MAX(EXAM_ID) FROM EXAMS_LIST");
+        int rowsBefore = 0;
+        int idToDelete = 0;
+        if (rs.next()) {
+            rowsBefore = rs.getInt("COUNT(EXAM_ID)");
+            idToDelete = rs.getInt("MAX(EXAM_ID)");
+        }
+        final int result = examImplUnderTest.delete(idToDelete);
+        rs = null;
+        int rowsAfter = 0;
+        rs = st.executeQuery("SELECT COUNT(EXAM_ID) FROM EXAMS_LIST");
+        if (rs.next()) {
+            rowsAfter = rs.getInt(1);
+        }
+        assertEquals(result , 1);
+        assertTrue(rowsAfter + 1 == rowsBefore);
     }
 
     @Test
     public void testGetNameById() {
-        // Setup
-
-        // Run the test
-        final String result = examImplUnderTest.getNameById(0);
-
-        // Verify the results
-        assertEquals("result" , result);
+        examsList = examImplUnderTest.getAll();
+        int idToGet = 0;
+        String expected = "";
+        if (examsList.size() > 0) {
+            idToGet = examsList.get(0).getExamId();
+            expected = examsList.get(0).getExamName();
+        }
+        final String result = examImplUnderTest.getNameById(idToGet);
+        assertEquals(result , expected);
     }
 }
