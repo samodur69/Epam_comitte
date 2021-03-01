1b662a73fe11a8864d85b1c7ef30316adbc29951
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-03-02 00:23:02 +0300

    add BeforeSuit method to create connection

--- src/test/java/dao/implementations/BaseTest.java
+++ src/test/java/dao/implementations/BaseTest.java
@@ -1,10 +1,27 @@
 package dao.implementations;
 
+import data.DBConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.sql.Connection;
+
 public abstract class BaseTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+    Connection conn;
+
     @BeforeSuite
     public void beforeSuitSetUp() {
-        System.out.println("BEFORE");
+        // there is place for make connection to test base, but i didn`t implements it
+        logger.info("Get connection for TestNg");
+        conn = DBConnection.getConnection();
+    }
+
+    @AfterSuite
+    public void shutdownTestConnection() {
+        DBConnection.close(conn);
     }
 }
