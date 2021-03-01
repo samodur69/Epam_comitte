c4fa7091d9805708d98959eb515fc62e06c908e6
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-03-01 18:28:47 +0300

    move enroll all and get total mark to Applicant Service Class

--- src/main/java/dao/interfaces/ApplicantDao.java
+++ src/main/java/dao/interfaces/ApplicantDao.java
@@ -9,8 +9,6 @@ public interface ApplicantDao extends BaseDao<Applicant> {
     Applicant getByEmail(String email);
     int getIdByEmail(String email);
     boolean checkEmailUnique(String email);
-    int getTotalMark(int id);
-    int enrollAllApplicants();
     List<Applicant> getByEnrolled();
 
 }
