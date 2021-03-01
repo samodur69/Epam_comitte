1a5b193d9045d98b133a7100b4710ce719d3d101
samodur69 <69460645+samodur69@users.noreply.github.com>
2021-03-01 23:21:06 +0300

    remove nullpointerexceptions with Optional

---
+++ src/main/java/util/AppException.java
@@ -0,0 +1,23 @@
+package util;
+
+public class AppException extends RuntimeException {
+
+    public AppException() {
+    }
+
+    public AppException(String message) {
+        super("Custom Exception" + message);
+    }
+
+    public AppException(String message , Throwable cause) {
+        super(message , cause);
+    }
+
+    public AppException(Throwable cause) {
+        super(cause);
+    }
+
+    public AppException(String message , Throwable cause , boolean enableSuppression , boolean writableStackTrace) {
+        super(message , cause , enableSuppression , writableStackTrace);
+    }
+}
